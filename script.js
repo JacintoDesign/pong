8601653933ff3557d7a81c2807a37fa4f34a82d4
JACINTO WONG <jacinto.wong@cbc.ca>
2020-03-25 10:31:36 -0400

    serving back and forth

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let height = 700;
 // Paddle
 let paddleHeight = 10;
 let paddleWidth = 50;
+let paddleDiff = 25;
 let paddle1_X = 225;
 let paddle2_X = 225;
 
@@ -61,19 +62,35 @@ function renderCanvas() {
 
 function ballUpdate() {
     // Vertical Speed
-    ball_Y += velocity_Y;
+    ball_Y += -velocity_Y;
     // Horizontal Speed
-    ball_X += velocity_X;
+    // ball_X += velocity_X;
 }
 
 function ballBoundaries() {
     // Bounce off Left Wall
     if (ball_X < 0 && velocity_X < 0) {
-        velocity_X = -velocity_X;
+        velocity_X =- velocity_X;
     }
     // Bounce off Right Wall
     if (ball_X > width && velocity_X > 0) {
-        velocity_X = -velocity_X;
+        velocity_X =- velocity_X;
+    }
+    // Bounce off player paddle (bottom), or award point if missed
+    if (ball_Y > height - paddleDiff) {
+        if (ball_X > paddle1_X && ball_X < paddle1_X + paddleWidth) {
+            velocity_Y = -velocity_Y;
+        } else {
+            console.log('computer Point');
+        }
+    }
+    // Bounce off computer paddle (top), or award point if missed
+    if (ball_Y < paddleDiff) {
+        if (ball_X > paddle1_X && ball_X < paddle1_X + paddleWidth) {
+            velocity_Y = -velocity_Y;
+        } else {
+            console.log('player Point');
+        }
     }
 }
 
